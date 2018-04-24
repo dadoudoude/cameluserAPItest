82a5df94a92f76b262a2fbc9378baaf30a4deace
dadoudoude <274717413@qq.com>
2018-04-24 16:16:26 +0800

    add mailaddress

--- report/send_email.py
+++ report/send_email.py
@@ -23,7 +23,6 @@ def sendEmail(content, title, from_name, from_address, to_address, serverport, s
 
 def main2():
     TO = ['274717413@qq.com']
-    TO = ['3346263535@qq.com']
     config = {
         "from": "773767639@qq.com",
         "from_name": '骆驼userAPI自动化测试报告:',
