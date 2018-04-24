0c149c440242ce0046a77260ead6acc114fc4318
dadoudoude <274717413@qq.com>
2018-04-24 15:44:10 +0800

    add mailaddress

--- report/send_email.py
+++ report/send_email.py
@@ -22,7 +22,7 @@ def sendEmail(content, title, from_name, from_address, to_address, serverport, s
 #HEFEN_D = pth.abspath(pth.dirname(__file__))
 
 def main2():
-    TO = ['274717413@qq.com']
+    TO = ['274717413@qq.com','liuguicen@druid.tech']
     config = {
         "from": "773767639@qq.com",
         "from_name": '骆驼userAPI自动化测试报告:',
